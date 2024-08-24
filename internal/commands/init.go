c486c5bdae089c6111cfc21bac1526160092cd13
kermage <genealyson.torcende@gmail.com>
2024-08-25 02:46:24 +0800

    we create our own gitignore file

--- internal/commands/init.go
+++ internal/commands/init.go
@@ -1,6 +1,9 @@
 package commands
 
 import (
+	"os"
+	"path/filepath"
+
 	"aagh/internal/helpers"
 
 	"github.com/spf13/cobra"
@@ -10,12 +13,20 @@ func init() {
 	rootCmd.AddCommand(&cobra.Command{
 		Use:   "init",
 		Short: "Initialize the repository in the current directory",
-		Run: func(_ *cobra.Command, _ []string) {
+		RunE: func(_ *cobra.Command, _ []string) error {
 			hooks := helpers.Hooks(helpers.ProjectRoot())
 
 			hooks.Config().Set()
 			helpers.CreateDir(hooks.Directory().FullPath())
 			helpers.CreateDir(hooks.Runner().FullPath())
+
+			err := os.WriteFile(filepath.Join(hooks.Runner().FullPath(), ".gitignore"), []byte("*"), 0644)
+
+			if err != nil {
+				return err
+			}
+
+			return nil
 		},
 	})
 }
