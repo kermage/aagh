d2114e5674157dcc436305e2598d8aecbbc812ce
kermage <genealyson.torcende@gmail.com>
2025-05-12 23:29:35 +0800

    add run command

---
+++ internal/commands/run.go
@@ -0,0 +1,53 @@
+package commands
+
+import (
+	"fmt"
+	"os"
+	"os/exec"
+	"path/filepath"
+	"strings"
+
+	"aagh/internal/helpers"
+
+	"github.com/kermage/GO-Mods/pathinfo"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	rootCmd.AddCommand(&cobra.Command{
+		Use:   "run [hook]",
+		Short: "Run a hook in the repository of current directory",
+		Args:  cobra.ExactArgs(1),
+		RunE: func(_ *cobra.Command, args []string) error {
+			hooks := helpers.Hooks(helpers.ProjectRoot())
+
+			if !hooks.ValidRoot() {
+				cobra.CheckErr(fmt.Sprintf("'%s' is not initialized.\n\nRun '%s init' first before setting up hooks.\n", hooks.Project().FullPath(), helpers.NAME))
+			}
+
+			hookName := args[0]
+			hookPath := pathinfo.Get(filepath.Join(hooks.Runner().FullPath(), hookName))
+
+			if !hookPath.Exists() {
+				cobra.CheckErr(fmt.Sprintf("'%[1]s' does not exists.\n\nRun '%[2]s setup %[1]s' first before running this command.\n", hookName, helpers.NAME))
+			}
+
+			cmd := exec.Command(hookPath.FullPath())
+			cmd.Dir = helpers.ProjectRoot()
+			res, err := cmd.CombinedOutput()
+
+			if err != nil {
+				fmt.Fprintln(os.Stderr, string(res))
+				os.Exit(helpers.GetExitCode(err))
+			}
+
+			msg := strings.TrimSpace(string(res))
+
+			if msg != "" {
+				fmt.Println(msg)
+			}
+
+			return nil
+		},
+	})
+}
